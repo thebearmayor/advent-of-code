b0a4b3c6bdb0d78bb59f895201d2da5e5d89dc98
Bradley Peterson <despite@gmail.com>
2022-12-04 20:47:15 +0000

    Day01

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,26 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(elves) do
+    elves
+    |> String.trim()
+    |> String.split("\n\n")
+    |> Enum.map(&calories/1)
+    |> Enum.max()
   end
 
-  def part2(_args) do
+  def part2(elves) do
+    elves
+    |> String.trim()
+    |> String.split("\n\n")
+    |> Enum.map(&calories/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  def calories(elf) do
+    elf
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 end
